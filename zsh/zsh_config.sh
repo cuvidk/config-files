2ed0025c662fcd658df4277a1539439c4609846a
Corvin Tiperciuc <cuvidk@gmail.com>
2021-01-18 15:07:07 +0200

    Small fix

--- zsh/zsh_config.sh
+++ zsh/zsh_config.sh
@@ -4,13 +4,13 @@ install() {(
     sed "s|ZSH_INSTALL_PATH|${PATH_OHMYZSH}|" "${MAKE_CONFIG_SCRIPT_DIR}/zsh/config/.zshrc" | 
         sed "s|PURE_INSTALL_PATH|${PATH_PURE}|" |
         tee "${PATH_ZSH_CONFIG}"
-    chsh -s /bin/zsh "${USER}"
+    chsh -s /usr/bin/zsh "${USER}"
     exit 0
 )}
 
 uninstall() {(
     set -e
     rm -rf "${PATH_ZSH_CONFIG}"
-    chsh -s /bin/bash "${USER}"
+    chsh -s /usr/bin/bash "${USER}"
     exit 0
 )}
