c0cd2e3b0158f0b5dcf4322eb1c3ef475916adaf
Corvin Tiperciuc <cuvidk@gmail.com>
2021-02-10 17:33:56 +0200

    Fixed golang installation

--- go/go_config.sh
+++ go/go_config.sh
@@ -1,7 +1,7 @@
 install() {(
     set -e
     mkdir -p "$(dirname ${PATH_GO_PROFILE})"
-    cp "${MAKE_CONFIG_SCRIPT_DIR}/go/config/go.sh" "${PATH_GO_PROFILE}"
+    sed "s|PATH_GOLANG|${PATH_GOLANG}|" "${MAKE_CONFIG_SCRIPT_DIR}/go/config/go.sh" >"${PATH_GO_PROFILE}"
     exit 0
 )}
 
